3d483807aa352fd8efd439eecf1f69fbf1e70052
Collins Masira Ondieki <collinsmasiraondieki@gmail.com>
2026-04-16 09:59:08 +0300

    Complete login tracker with closure and attempt limits

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
+    let locked = false;
 
+    const loginAttempt = (passwordAttempt) => {
+        if (locked) {
+            return "Account locked due to too many failed login attempts";
+        }
 
+        attemptCount++;
+
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        }
+
+        // ❗ allow 3 failed attempts FIRST
+        if (attemptCount >= 4) {
+            locked = true;
+            return "Account locked due to too many failed login attempts";
+        }
+
+        return `Attempt ${attemptCount}: Login failed`;
+    };
+
+    return loginAttempt;
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+    createLoginTracker
 };
